edb1ef44cb51331629584e7fb28dbf8b830e2ee2
vagrant <vagrant@homestead>
2017-11-17 12:11:01 +0000

    NasaApiTest

---
+++ tests/Feature/Feature/NasaApiTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature\Feature;
+
+use App\ExternalApi\Nasa;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class NasaApiTest extends TestCase
+{
+    public function testApiWorkflow()
+    {
+        $nasa = new Nasa();
+        $response = $nasa->getNeo();
+        $this->assertTrue(is_array($response));
+        $this->assertTrue(!empty($response));
+    }
+}
