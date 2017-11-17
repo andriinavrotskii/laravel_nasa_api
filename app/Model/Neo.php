5948f100bbf2260a0ec6b9d4f779a7f05124e16f
vagrant <vagrant@homestead>
2017-11-17 16:23:15 +0000

    add  to Neo model, and some ApiTest methods

--- app/Model/Neo.php
+++ app/Model/Neo.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Neo extends Model
 {
-    protected $fillable = ['reference', 'date', 'name', 'speed', 'is_hazardous']; 
+    protected $fillable = ['reference', 'date', 'name', 'speed', 'is_hazardous'];
+
+    protected $hidden = ['id', 'created_at', 'updated_at'];
 }
--- tests/Feature/Api/ApiTest.php
+++ tests/Feature/Api/ApiTest.php
@@ -8,45 +8,87 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ApiTest extends TestCase
 {
-
-    public function testApi()
+    private function createNeos()
     {
-        $neo = factory(Neo::class)->create([
+        factory(Neo::class)->create([
             'reference' => 1111111,
             'date' => '2017-11-16',
             'name' => '1111111',
             'speed' => 111111.1111111111,
             'is_hazardous' => 0,
         ]);
-        $neo = factory(Neo::class)->create([
+        factory(Neo::class)->create([
             'reference' => 2222222,
             'date' => '2017-11-16',
             'name' => '2222222',
             'speed' => 222222.2222222222,
             'is_hazardous' => 1,
         ]);
-        $neo = factory(Neo::class)->create([
+        factory(Neo::class)->create([
             'reference' => 3333333,
             'date' => '2017-11-16',
             'name' => '3333333',
             'speed' => 333333.3333333333,
             'is_hazardous' => 0,
         ]);
+    }
+
+
+    public function testApiHazardous()
+    {
+        $this->createNeos();
 
         $response = $this->json('GET', '/api/neo/hazardous')
             ->assertStatus(200)
             ->assertJsonCount(1)
             ->assertJsonStructure([
-                '*' => ['id', 'reference', 'date', 'name', 'speed', 'is_hazardous']
+                '*' => ['reference', 'date', 'name', 'speed', 'is_hazardous']
             ])
             ->assertJson([
-                'reference' => 2222222,
-                'date' => '2017-11-16',
-                'name' => '2222222',
-                'speed' => 222222.2222222222,
-                'is_hazardous' => 1,
+                [
+                    'reference' => 2222222,
+                    'date' => '2017-11-16',
+                    'name' => '2222222',
+                    'speed' => 222222.2222222222,
+                    'is_hazardous' => 1,
+                ]
             ]);
 
-        dd($response->getReference());
+    }
+
+    public function testApiFastestNonHazardous()
+    {
+        $this->createNeos();
+
+        $response = $this->json('GET', '/api/neo/fastest?hazardous=false')
+            ->assertStatus(200)
+            ->assertJsonStructure(['reference', 'date', 'name', 'speed', 'is_hazardous'])
+            ->assertJson([
+                'reference' => 3333333,
+                'date' => '2017-11-16',
+                'name' => '3333333',
+                'speed' => 333333.3333333333,
+                'is_hazardous' => 0,
+            ]);
+    }
+
+    public function testApiFastestHazardous()
+    {
+        $this->createNeos();
+
+        $fourthNeo = [
+            'reference' => 4444444,
+            'date' => '2017-11-16',
+            'name' => '4444444',
+            'speed' => 4444444.4444444444,
+            'is_hazardous' => 1,
+        ];
+
+        factory(Neo::class)->create($fourthNeo);
+
+        $response = $this->json('GET', '/api/neo/fastest?hazardous=true')
+            ->assertStatus(200)
+            ->assertJsonStructure(['reference', 'date', 'name', 'speed', 'is_hazardous'])
+            ->assertJson($fourthNeo);
     }
 }
